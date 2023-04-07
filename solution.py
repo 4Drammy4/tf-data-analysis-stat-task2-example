3b9e6f8752cecba76d2575a8246bb952a20d0381
4Drammy4 <90786830+4Drammy4@users.noreply.github.com>
2023-04-07 19:52:13 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,20 +1,8 @@
-import numpy as np
 
-from scipy import stats
 
 
 chat_id = 416934694 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    n = len(x)
-    mean = np.mean(x)
-    std = np.std(x, ddof=1)
-    t = stats.t.ppf((1 + p) / 2, n - 1)
-    margin_of_error = t * std / np.sqrt(n)
-    lower = mean - margin_of_error
-    upper = mean + margin_of_error
-
-    alpha = 1 - p
-    x = alpha / 2 + 0.092
-    b = (alpha - 0.092) / x + 0.092
-    return (lower, upper)
+    return max(x), \
+           (max(x))/(alpha**(1/len(x)))
