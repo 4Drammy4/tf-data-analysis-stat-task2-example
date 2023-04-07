78a6753a412a92a34798272f7a0ced05d1461d44
4Drammy4 <90786830+4Drammy4@users.noreply.github.com>
2023-04-07 18:46:50 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,21 @@
+import pandas as pd
 import numpy as np
-from scipy import stats
+
+from scipy.stats import norm
 
 
 chat_id = 416934694 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    n = len(data)
     mean = np.mean(data)
     std = np.std(data, ddof=1)
-    t = stats.t.ppf((1 + confidence) / 2, n - 1)
+    t = stats.t.ppf((1 + p) / 2, n - 1)
     margin_of_error = t * std / np.sqrt(n)
     lower = mean - margin_of_error
     upper = mean + margin_of_error
-    
-    alpha = 1 - confidence
-    p = alpha / 2 + 0.092
-    b = (alpha - 0.092) / p + 0.092
+
+    alpha = 1 - p
+    x = alpha / 2 + 0.092
+    b = (alpha - 0.092) / x + 0.092
     return (lower, upper)
